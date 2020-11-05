27f8677525a886fcfcca21fb1f5deac930daae71
Ali Nazari <ali4nazari4@gmail.com>
2020-11-06 00:29:15 +0330

    feat: improve high school teams registration status

--- src/components/RegistrationStatus/ListOfTeamsStatus.js
+++ src/components/RegistrationStatus/ListOfTeamsStatus.js
@@ -1,47 +1,94 @@
-import React from "react"
-import SituationCard from "./SituationCard"
-import "../styles/situation-card.css"
+import React from "react";
+import SituationCard from "./SituationCard";
+import "../styles/situation-card.css";
 
-class ListOfTeamsStatus extends React.Component { 
-    constructor() { 
-        super() 
-        this.state = {
-            boxShadow: "0 4px 8px 0 rgba(0, 0, 0, 0.7)",
-        }
-    }
-   
+class ListOfTeamsStatus extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      boxShadow: "0 4px 8px 0 rgba(0, 0, 0, 0.7)",
+    };
+  }
 
-    render() {
-        const cards = this.props.data.map((cardData) => <div className = "cards-container" ><SituationCard data = {cardData}/></div>)
-        return (     
-            <div className = "super-status-cards-container" >  
-                
-                    <div className = "help-container">
-                    {/* <h1>OnSite Registered Teams</h1> */}
-                            <h1>Registered Teams</h1>
-                            <p>
-                                {/* Here is the list of OnSite registered teams.<br/> */}
-                                Here is the list of registered teams.<br/>
-                                If your team name was not in registerd team list 24 hours after regitration, please inform us by:<br/> 
-                                Email: <a href="mailto:ceit.ssc94@gmail.com">ceit.ssc94@gmail.com</a><br/>
-                                Telegram: <a href="https://t.me/ceitssc">@ceitssc</a> <br/>
-                            </p>
-                            <div className = "help-box">
-                                <div className = "help"> <div className = "color-circle" style = {{backgroundColor : "#6b6b6b"}}/> <p>  Pending Approval</p></div>
-                                <div className = "help"> <div className = "color-circle" style = {{backgroundColor : "#3c9440"}}/> <p>  Approved for payment</p></div>
-                                <div className = "help"> <div className = "color-circle" style = {{backgroundColor : "#3065a3"}}/> <p>  Finalized</p></div>
-                                <div className = "help"> <div className = "color-circle" style = {{backgroundColor : "#aa740b"}}/> <p>  Reserved registration beforehand</p></div>
-                                <div className = "help"> <div className = "color-circle" style = {{backgroundColor : "#e23838"}}/> <p>  Denied Participation</p></div>
-                            </div>
-                    </div>
-              
-               
-                <div className = "status-cards-container">
-                    {cards}
-                </div>
+  render() {
+    const cards = this.props.data.map((cardData) => (
+      <div key={cardData.name} className="cards-container">
+        <SituationCard data={cardData} />
+      </div>
+    ));
+    return (
+      <div className="super-status-cards-container">
+        <div className="help-container">
+          {/* <h1>OnSite Registered Teams</h1> */}
+          <h1>Registered Teams</h1>
+          <p>
+            {/* Here is the list of OnSite registered teams.<br/> */}
+            Here is the list of registered teams.
+            <br />
+            If your team name was not in registerd team list 24 hours after
+            regitration, please inform us by:
+            <br />
+            Email:{" "}
+            <a href="mailto:ceit.ssc94@gmail.com">ceit.ssc94@gmail.com</a>
+            <br />
+            Telegram: <a href="https://t.me/ceitssc">@ceitssc</a> <br />
+          </p>
+          <div className="help-box">
+            <div className="help">
+              {" "}
+              <div
+                className="color-circle"
+                style={{ backgroundColor: "#6b6b6b" }}
+              />{" "}
+              <p> Pending Approval</p>
             </div>
-        )
-    }
+            <div className="help">
+              {" "}
+              <div
+                className="color-circle"
+                style={{ backgroundColor: "#3c9440" }}
+              />{" "}
+              <p> Approved for payment</p>
+            </div>
+            <div className="help">
+              {" "}
+              <div
+                className="color-circle"
+                style={{ backgroundColor: "#3065a3" }}
+              />{" "}
+              <p> Finalized</p>
+            </div>
+            <div className="help">
+              {" "}
+              <div
+                className="color-circle"
+                style={{ backgroundColor: "#aa740b" }}
+              />{" "}
+              <p> Reserved registration beforehand</p>
+            </div>
+            <div className="help">
+              {" "}
+              <div
+                className="color-circle"
+                style={{ backgroundColor: "#e23838" }}
+              />{" "}
+              <p> Denied Participation</p>
+            </div>
+            <div className="help">
+              {" "}
+              <div
+                className="color-circle"
+                style={{ backgroundColor: "#000000" }}
+              />{" "}
+              <p> High School Team</p>
+            </div>
+          </div>
+        </div>
+
+        <div className="status-cards-container">{cards}</div>
+      </div>
+    );
+  }
 }
 
-export default ListOfTeamsStatus
+export default ListOfTeamsStatus;
--- src/components/RegistrationStatus/SituationCard.js
+++ src/components/RegistrationStatus/SituationCard.js
@@ -1,70 +1,93 @@
-import React from "react"
-import "../styles/situation-card.css"
+import React from "react";
+import "../styles/situation-card.css";
 
-class SituationCard extends React.Component { 
-    constructor() { 
-        super() 
-        this.state = { 
-            style : {boxShadow: "0 10px 10px 0 rgba(0, 0, 0, 0.4)",backgroundColor : "black"},
-            dict : {
-                    'PENDING': 'Pending Approval',
-                    'APPROVED': 'Approved for payment',
-                    'PAID': 'Finalized',
-                    'REJECTED': 'Denied Participation',
-                    'RESERVED': 'Reserved registration beforehand'
-            }
-        }
-    }
+class SituationCard extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      style: {
+        boxShadow: "0 10px 10px 0 rgba(0, 0, 0, 0.4)",
+        backgroundColor: "black",
+      },
+      dict: {
+        PENDING: "Pending Approval",
+        APPROVED: "Approved for payment",
+        PAID: "Finalized",
+        REJECTED: "Denied Participation",
+        RESERVED: "Reserved registration beforehand",
+      },
+    };
+  }
 
-    componentDidMount () { 
-        let color = ""
-        if(this.props.data.status === "REJECTED")
-                 color = "#e23838"
-        else if (this.props.data.status === "APPROVED")
-                 color = "#3c9440"
-        else if (this.props.data.status === "PENDING")
-                 color = "#6b6b6b"
-        else if (this.props.data.status === "PAID")
-                 color = "#3065a3"
-        else if (this.props.data.status === "RESERVED")
-                 color = "#aa740b"
-        
-        this.setState({
-            style : {
-                boxShadow: "0 10px 10px 0 rgba(0, 0, 0, 0.4)",
-                backgroundColor : color
-            }
-        })
-    }
+  componentDidMount() {
+    let color = "";
+    if (this.props.data.status === "REJECTED") color = "#e23838";
+    else if (this.props.data.status === "APPROVED") color = "#3c9440";
+    else if (this.props.data.status === "PENDING") color = "#6b6b6b";
+    else if (this.props.data.status === "PAID") color = "#3065a3";
+    else if (this.props.data.status === "RESERVED") color = "#aa740b";
 
-    render() {
-        let color = ""
-        if(this.props.data.status === "REJECTED")
-                 color = "#e23838"
-        else if (this.props.data.status === "APPROVED")
-                 color = "#3c9440"
-        else if (this.props.data.status === "PENDING")
-                 color = "#6b6b6b"
-        else if (this.props.data.status === "PAID")
-                 color = "#3065a3"
-        else if (this.props.data.status === "RESERVED")
-                 color = "#aa740b"
-            
-        return (
-            <div className ="situation-card"
-                 style = {this.state.style}
-                 onMouseEnter = {() =>
-                    {this.setState({style :
-                            {boxShadow: "0 10px 10px 0 rgba(0, 0, 0, 0.7)",
-                                backgroundColor : color}})}}
-                                onMouseLeave = {() => {this.setState({style : {boxShadow: "0 10px 10px 0 rgba(0, 0, 0, 0.4)",
-                                        backgroundColor : color}})}} >
-                <h1>{this.props.data.name} ‍‍{`${this.props.data.is_high ? "(High School Team)" : ""}`}</h1>
-                <p className = "state">State: <span>{this.state.dict[this.props.data.status]}</span></p>
-                <p>Institution: <span>{this.props.data.institution}</span></p>
-            </div>
-        )
-    }
+    this.setState({
+      style: {
+        boxShadow: "0 10px 10px 0 rgba(0, 0, 0, 0.4)",
+        backgroundColor: color,
+      },
+    });
+  }
+
+  render() {
+    let color = "";
+    if (this.props.data.status === "REJECTED") color = "#e23838";
+    else if (this.props.data.status === "APPROVED") color = "#3c9440";
+    else if (this.props.data.status === "PENDING") color = "#6b6b6b";
+    else if (this.props.data.status === "PAID") color = "#3065a3";
+    else if (this.props.data.status === "RESERVED") color = "#aa740b";
+
+    return (
+      <div
+        className="situation-card"
+        style={this.state.style}
+        onMouseEnter={() => {
+          this.setState({
+            style: {
+              boxShadow: "0 10px 10px 0 rgba(0, 0, 0, 0.55)",
+              backgroundColor: color,
+            },
+          });
+        }}
+        onMouseLeave={() => {
+          this.setState({
+            style: {
+              boxShadow: "0 10px 10px 0 rgba(0, 0, 0, 0.4)",
+              backgroundColor: color,
+            },
+          });
+        }}
+      >
+        <div>
+          <h1 style={{ margin: "0", marginBottom: "5px" }}>
+            {this.props.data.name}
+          </h1>
+          <p className="state">
+            State: <span>{this.state.dict[this.props.data.status]}</span>
+          </p>
+          <p>
+            Institution: <span>{this.props.data.institution}</span>
+          </p>
+        </div>
+        {this.props.data.is_high ? (
+          <div
+            style={{
+              height: "100%",
+              width: "20px",
+              backgroundColor: "#000000",
+              borderRadius: "0 3px 5px 0",
+            }}
+          />
+        ) : null}
+      </div>
+    );
+  }
 }
 
-export default SituationCard 
+export default SituationCard;
