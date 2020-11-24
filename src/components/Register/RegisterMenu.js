4468aa54a16420e777ea2a0ed07b4bf075da9f51
Ali Nazari <ali4nazari4@gmail.com>
2020-11-24 21:36:23 +0330

    close registration

--- src/components/Register/RegisterMenu.js
+++ src/components/Register/RegisterMenu.js
@@ -1,71 +1,69 @@
-import React from "react"
-import { NavLink } from 'react-router-dom'
-import "../styles/menu.css"
+import React from "react";
+import { NavLink } from "react-router-dom";
+import "../styles/menu.css";
 
-export default class RegisterMenu extends React.Component { 
-    constructor () { 
-        super()  
-        this.state = { 
-            style1: { 
-                transform : "scale(1)",
-                transitionProperty :"transform", 
-                transitionDuration: "200ms",
-            }
-            , 
-            style2: { 
-                transform : "scale(1)",
-                transitionProperty :"transform", 
-                transitionDuration: "200ms",
-            }
+export default class RegisterMenu extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      style1: {
+        transform: "scale(1)",
+        transitionProperty: "transform",
+        transitionDuration: "200ms",
+      },
+      style2: {
+        transform: "scale(1)",
+        transitionProperty: "transform",
+        transitionDuration: "200ms",
+      },
+    };
+    this.onhover = this.onhover.bind(this);
+    this.onleave = this.onleave.bind(this);
+    this.onhover2 = this.onhover2.bind(this);
+    this.onleave2 = this.onleave2.bind(this);
+  }
+  onhover() {
+    this.setState({
+      style1: {
+        transform: "scale(1.1)",
+        transitionProperty: "transform",
+        transitionDuration: "200ms",
+      },
+    });
+  }
+  onleave() {
+    this.setState({
+      style1: {
+        transform: "scale(1)",
+        transitionProperty: "transform",
+        transitionDuration: "200ms",
+      },
+    });
+  }
+  onhover2() {
+    this.setState({
+      style2: {
+        transform: "scale(1.1)",
+        transitionProperty: "transform",
+        transitionDuration: "200ms",
+      },
+    });
+  }
+  onleave2() {
+    this.setState({
+      style2: {
+        transform: "scale(1)",
+        transitionProperty: "transform",
+        transitionDuration: "200ms",
+      },
+    });
+  }
 
-        }
-        this.onhover = this.onhover.bind(this)
-        this.onleave = this.onleave.bind(this) 
-        this.onhover2 = this.onhover2.bind(this)
-        this.onleave2 = this.onleave2.bind(this)
-    }
-    onhover() { 
-            this.setState({
-                style1: { 
-                    transform : "scale(1.1)",
-                    transitionProperty :"transform", 
-                    transitionDuration: "200ms",
-                }
-            })
-    }
-    onleave(){
-            this.setState({
-                style1: { 
-                    transform : "scale(1)",
-                    transitionProperty :"transform", 
-                    transitionDuration: "200ms",
-                }
-            })
-    }
-    onhover2() { 
-        this.setState({
-            style2: { 
-                transform : "scale(1.1)",
-                transitionProperty :"transform", 
-                transitionDuration: "200ms",
-            }
-        })
-    }
-    onleave2(){
-            this.setState({
-                style2: { 
-                    transform : "scale(1)",
-                    transitionProperty :"transform", 
-                    transitionDuration: "200ms",
-                }
-            })
-    }
-
-    render() {
-        return(
-            <div className="menu-container">
-                <div className="card-container" >
-                    {/* <NavLink className="nav-link"
+  render() {
+    return (
+      <div className="menu-container">
+        <div className="card-container">
+          {/* <NavLink className="nav-link"
                         // onClick={() => alert("On-Site Registration has been Closed! You can register in Online contest.")}
                          onClick={() => window.scrollTo(0, 0)}
                          exact to="/onsiteRegister"
@@ -88,7 +86,7 @@ export default class RegisterMenu extends React.Component {
                             </div>
                         </div>
                     </NavLink> */}
-                    {/* <NavLink className="nav-link"
+          {/* <NavLink className="nav-link"
                      //   onClick={() => alert("Registration has been Closed!")//   }
                         onClick={() => window.scrollTo(0, 0)}
                         exact to="/onlineRegister"
@@ -111,52 +109,60 @@ export default class RegisterMenu extends React.Component {
                             </div>
                         </div>
                     </NavLink> */}
-                    <NavLink className="nav-link"
-                         onClick={() => window.scrollTo(0, 0)}
-                         exact to="/onlineCovidRegisterUni"
-                    >
-                        <div className ="register-card"
-                            onMouseEnter={this.onhover} 
-                            onMouseLeave={this.onleave}
-                            style={this.state.style1}    
-                        >
-                            <div className="card-image">
-                                <img src={require("./../../img/online.svg")}
-                                    width="200" height="200"
-                                     alt="onsite"
-                                />
-                            </div>  
-                            <div className="card-text">
-                                <p>
-                                    University Students
-                                </p>
-                            </div>
-                        </div>
-                    </NavLink>
-                    <NavLink className="nav-link"
-                         onClick={() => window.scrollTo(0, 0)}
-                         exact to="/onlineCovidRegister"
-                    >
-                        <div className ="register-card"
-                            onMouseEnter={this.onhover2} 
-                            onMouseLeave={this.onleave2}
-                            style={this.state.style2}    
-                        >
-                            <div className="card-image">
-                                <img src={require("./../../img/online.svg")}
-                                    width="200" height="200"
-                                     alt="onsite"
-                                />
-                            </div>  
-                            <div className="card-text">
-                                <p>
-                                    School Students
-                                </p>
-                            </div>
-                        </div>
-                    </NavLink>
-                </div>
+          <NavLink
+            className="nav-link"
+            //  onClick={() => window.scrollTo(0, 0)}
+            onClick={() => alert("Registration has been Closed!")}
+            exact
+            // to="/onlineCovidRegisterUni"
+          >
+            <div
+              className="register-card"
+              onMouseEnter={this.onhover}
+              onMouseLeave={this.onleave}
+              style={this.state.style1}
+            >
+              <div className="card-image">
+                <img
+                  src={require("./../../img/online.svg")}
+                  width="200"
+                  height="200"
+                  alt="onsite"
+                />
+              </div>
+              <div className="card-text">
+                <p>University Students</p>
+              </div>
             </div>
-        )
-    }
+          </NavLink>
+          <NavLink
+            className="nav-link"
+            //  onClick={() => window.scrollTo(0, 0)}
+            onClick={() => alert("Registration has been Closed!")}
+            exact
+            // to="/onlineCovidRegister"
+          >
+            <div
+              className="register-card"
+              onMouseEnter={this.onhover2}
+              onMouseLeave={this.onleave2}
+              style={this.state.style2}
+            >
+              <div className="card-image">
+                <img
+                  src={require("./../../img/online.svg")}
+                  width="200"
+                  height="200"
+                  alt="onsite"
+                />
+              </div>
+              <div className="card-text">
+                <p>School Students</p>
+              </div>
+            </div>
+          </NavLink>
+        </div>
+      </div>
+    );
+  }
 }
